1529f1c1140250c070f9417a6e0c7705ce0c522b
Scrutinizer Auto-Fixer <auto-fixer@scrutinizer-ci.com>
2017-11-28 01:51:02 +0000

    Scrutinizer Auto-Fixes
    This commit consists of patches automatically generated for this project on https://scrutinizer-ci.com

--- srcAPI/validation/userValidator.php
+++ srcAPI/validation/userValidator.php
@@ -7,32 +7,37 @@ class UserValidator implements IUserValidator
 {
     public function isNameValid($name)
     {
-        if (count($name) > 40)
-            return false;
-        if (is_numeric($name[0]))
-            return false;
+        if (count($name) > 40) {
+                    return false;
+        }
+        if (is_numeric($name[0])) {
+                    return false;
+        }
         return true;
     }
 
     public function isregisternumberValid($registernumber)
     {
-        if (!is_numeric($registernumber))
-            return false;
-        if (ctype_space($registernumber))
-            return false;
+        if (!is_numeric($registernumber)) {
+                    return false;
+        }
+        if (ctype_space($registernumber)) {
+                    return false;
+        }
         return true;
     }
 
     public function isUsernameValid($username)
     {
-        if (count($username) > 15)
-            return false;
+        if (count($username) > 15) {
+                    return false;
+        }
         return true;
     }
 
     public function isemailValid($email)
     {
-        if(filter_var($email, FILTER_VALIDATE_EMAIL)){
+        if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
         return true;
         }
         return false;
@@ -40,7 +45,7 @@ class UserValidator implements IUserValidator
 
     public function isPasswordValid($password) 
     {
-        if (count($password) > 20 ) 
+        if (count($password) > 20) 
             return false;
         return true;
     }
